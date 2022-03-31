a350d4c6980253d33ed97269165d4aa7c5194944
Alain Benbassat <alain@businessandcode.eu>
2022-03-31 16:29:18 +0200

    contactype

--- CRM/Muntpuntconfig/ConfigItems.php
+++ CRM/Muntpuntconfig/ConfigItems.php
@@ -3,6 +3,9 @@
 class CRM_Muntpuntconfig_ConfigItems {
 
   public static function load() {
+    // create the contact sub types first
+    self::createContactSubTypes();
+
     $resourcePath = Civi::resources()->getPath('be.muntpunt.muntpuntconfig') . '/resources';
 
     $result = civicrm_api3('Civiconfig', 'load_json', [
@@ -15,5 +18,21 @@ class CRM_Muntpuntconfig_ConfigItems {
 
   private static function addContactIdentity_oldCiviCRMId() {
     CRM_Identitytracker_Configuration::add_identity_type(CRM_Muntpuntconfig_Config::ICONTACT_ID_TYPE, 'Oude CiviCRM ID (iContact)');
+  }
+
+  private static function createContactSubTypes() {
+    $params = [
+      'name' => 'perspartner',
+      'label' => 'Perspartner',
+      'parent_id' => 3,
+    ];
+    civicrm_api3('ContactType', 'create', $params);
+
+    $params = [
+      'name' => 'persmedewerker',
+      'label' => 'Persmedewerker',
+      'parent_id' => 1,
+    ];
+    civicrm_api3('ContactType', 'create', $params);
   }
 }
